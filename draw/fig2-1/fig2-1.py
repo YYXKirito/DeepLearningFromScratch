4c70285e04350e02ee80031bb18477156e4ad475
YYX <yyxistopone@outlook.com>
2019-01-22 18:52:19 +0800

    build figure folder,add fig2-1

---
+++ draw/fig2-1/fig2-1.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.xlim((-2,2))
+plt.ylim((-2,2))
+
+plt.xlabel('x1')
+plt.ylabel('x2')
+
+plt.grid()
+
+axis = np.arange(-2,3,1)
+plt.plot(axis, [0]*axis.size, color='black')
+plt.plot([0]*axis.size, axis, color='black')
+
+x1 = np.array([0,0,1])
+x2 = np.array([0,1,0])
+plt.scatter(x1,x2, color='red')
+plt.scatter([1],[1], color='green')
+
+x = np.arange(-3,3,1)
+y = -x+1.4
+plt.plot(x,y)
+plt.show()
